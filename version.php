35d7b287226d10f3bd0abe3923de25c9405e3f5f
Itamar Tzadok <itamar@substantialmethods.com>
2016-06-08 09:25:19 -0400

    Release 3.0.4

--- version.php
+++ version.php
@@ -27,9 +27,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2015111603.02;
-$plugin->release = '3.0.4 rc1';
+$plugin->version = 2015111604;
+$plugin->release = '3.0.4';
 $plugin->requires = 2015111600;
 $plugin->cron = 0;
 $plugin->component = 'block_mhaairs';
-$plugin->maturity  = MATURITY_BETA;
+$plugin->maturity  = MATURITY_STABLE;
