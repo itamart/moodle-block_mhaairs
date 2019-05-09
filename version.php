d22ab699b455741c2364ce38a19047375708624d
Itamar Tzadok <admin@substantialmethods.com>
2019-05-08 23:47:55 -0400

    Version update: 3.6.3+ (20190508)

--- version.php
+++ version.php
@@ -28,7 +28,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2018120303;
-$plugin->release = '3.6.3';
+$plugin->release = '3.6.3+ (20190508)';
 $plugin->requires = 2018120300;
 $plugin->cron = 0;
 $plugin->component = 'block_mhaairs';
