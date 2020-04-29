9b46fcf8ea63513b26b85eb07e56883d441f183a
Itamar Tzadok <admin@substantialmethods.com>
2020-04-28 22:47:27 -0400

    Release 3.6.9

--- version.php
+++ version.php
@@ -18,7 +18,7 @@
  * Block MHAAIRS version
  *
  * @package block_mhaairs
- * @copyright 2019 Itamar Tzadok {@link http://substantialmethods.com}
+ * @copyright 2020 Itamar Tzadok {@link http://substantialmethods.com}
  * @copyright 2013-2014 Moodlerooms inc.
  * @author Teresa Hardy <thardy@moodlerooms.com>
  * @author Darko Miletic <dmiletic@moodlerooms.com>
@@ -28,8 +28,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2018120309;
-$plugin->release = '3.6.9 RC2';
+$plugin->release = '3.6.9';
 $plugin->requires = 2018120300;
 $plugin->cron = 0;
 $plugin->component = 'block_mhaairs';
-$plugin->maturity  = MATURITY_BETA;
+$plugin->maturity  = MATURITY_STABLE;
